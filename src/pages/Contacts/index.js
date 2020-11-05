69af673479070d725536b152b7ce019b53292bd2
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-11-05 20:30:54 +0700

    create table with contactsdata  fetched from api

---
+++ src/pages/Contacts/index.js
@@ -0,0 +1,47 @@
+import Container from "@material-ui/core/Container";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { useContacts } from "./useContacts";
+import { ContactsTable } from "../ContactsTable";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    headContainer: {
+      marginBottom: theme.spacing(3),
+    },
+  })
+);
+
+export const Contacts = () => {
+  const classes = useStyles();
+  const contacts = useContacts();
+
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h3" component="h1">
+            Contacts
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <div>...loading</div>;
+            }
+
+            if (contacts.isError) {
+              return <div>...error</div>;
+            }
+            return <ContactsTable data={contacts.data} />;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
